46eac78b35f23655c683b83aeeb95b9be0c16589
dilipdevops5511 <dilippalani052001@gmail.com>
2024-07-15 23:54:23 +0530

    v7

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 # Include VPC configuration
 module "vpc" {
-  source = "./vpc"
+  source = "vpc"
 }
 
 # Include EKS configuration
 module "eks" {
-  source = "./eks"
+  source = "eks"
 }
