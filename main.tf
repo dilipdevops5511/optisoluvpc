54ee8b57f616438dbd55c6f2ed19980d06113fbd
dilipdevops5511 <dilippalani052001@gmail.com>
2024-07-16 23:27:11 +0530

    finally eks working

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ resource "aws_subnet" "public_subnet_1" {
   }
 }
 
+
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.2.0/24"
