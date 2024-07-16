f8b747b91805ffcb89eb04a9491b6ae30082dc51
dilipdevops5511 <dilippalani052001@gmail.com>
2024-07-16 11:30:03 +0530

    new V1

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_role_attachment" {
 
 # EKS Cluster
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "my-eks-cluster"
+  name     = "opti-sol-eks"
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
